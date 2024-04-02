f39669d72b7bae3ed76638f53bfc861ecb1eae5b
Ephraim Okoye <theokoyes@gmail.com>
2024-04-02 18:25:30 -0400

    first commit

---
+++ module2/js-template/js/script.js
@@ -0,0 +1,49 @@
+
+console.log('Hello World');
+console.log("Hello World!");
+
+/**
+ * 
+ * This  is a multiline comment
+ */
+
+console.log(`
+    Hello
+    World!
+`);
+
+  // === variable  ======
+
+  // variable declaration
+var message = 'Welcome!';
+let user1 = "Ephraim";
+let user2 = "Okoye";
+const username = 'Chris11';
+
+
+// undefined variable
+let userInput;
+
+// variable assignment
+userInput = 'Sara';
+console.log(userInput);
+
+// userInput = prompt('Enter your name:');
+console.log(userInput);
+
+console.log(message);
+console.log(user1);
+console.log(user2);
+console.log(userInput);
+
+let postViews = 0;
+postViews = 1;
+postViews = 2;
+
+console.log(postViews);
+
+
+
+let result3 = 3 > 2 || 6 == 6;
+
+
